631dd256b2f75bf20d04fca06052d4f5a2123b6d
Anastasia <amityutneva@mail.ru>
2024-02-08 13:57:55 +0300

    lab3 ex2 changed

--- src/lab3/ex2.cpp
+++ src/lab3/ex2.cpp
@@ -2,25 +2,39 @@
 #include <string>
 #include <iostream>
 using namespace std;
-
-int ex() {
-    auto map = [](vector<int>* v, int (*fun_ptr)(int)) -> vector<int>* {
-        vector<int>* output;
-        for(int n : *v) {
-            output->push_back(fun_ptr(n));
-        }
-        return output;
-    };
-    vector<int> v = {1, 2, 3};
-    auto increase = [](int x) {return x+1;};
-    vector<int> output = map(v, increase);
-    for (int o : output) {
-        cout << o << endl;
+template <typename T, typename E>
+vector<E> customMap (vector<T> input, E (*func)(T)) {
+    vector<E> result = vector<E>();
+    for (T n : input) {
+        result.push_back(func(n));
     }
-    return 0;
+    return result;
 
 }
+
+template <typename T>
+vector<T> customFilter (vector<T> input, bool (*func)(T)) {
+    vector<T> result = vector<T>();
+    for (T n : input) {
+        if (func(n)) {
+            result.push_back(n);
+        }
+    }
+    return result;
+
+}
+
 int main() {
-    ex();
+    vector<int> nums = {1, 2, 3, 4, 5};
+    cout << "Squared: ";
+    auto squared = customMap<int, int>(nums, [](int x) { return x * x; });
+    for (int n : squared) {
+        cout << n << " ";
+    }
+    cout << endl << "Odds: ";
+    auto odds = customFilter<int>(nums, [](int x) { return x % 2 != 0; });
+    for (int n : odds) {
+        cout << n << " ";
+    }
     return 0;
 }
